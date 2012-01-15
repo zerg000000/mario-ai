acf8e2a4cee7df7bbf0ebd558d648538699a3446
albert <kayulai2@gmail.com>
2012-01-15 20:59:58 +0800

    fix clojure project compile problem

--- example/mario-ai-example-clj/project.clj
+++ example/mario-ai-example-clj/project.clj
@@ -2,4 +2,7 @@
   :description "Example Project for Mario AI in clojure, you can use it as template project."
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [ch.idsia/mario-ai "0.0.1-SNAPSHOT"]]
+  :repositories {"mario-ai repos" "http://zerg000000.github.com/mario-ai/repository"}
+  ;need to specify namespace which have gen-class
+  :aot [mario-ai-example-clj.agents]
   :main mario-ai-example-clj.core)
